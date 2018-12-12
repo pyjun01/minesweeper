f4882d5ac83e3ddb3cb261b67311638dc4d50a0b
pyjun01 <pyjun01@naver.com>
2018-12-12 11:09:00 +0900

    ~

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ function App() {
     this.M_lenX=0;
     this.M_lenY=0;
     this.M_len=0;
+
+    this.leftClick= false;
 }
 /* 난이도 선택 */
 App.prototype.eventInit = function() {
     var self= this;
+    window.oncontextmenu= function (e){ e.preventDefault();}
     var btn= document.getElementById("btn");
     btn.addEventListener("click", function (){
         switch (document.querySelector("input[type=radio]:checked").value) {
@@ -32,8 +35,7 @@ App.prototype.SetLength = function(x, y) {// set Array length, addHTML
     this.M_lenX= x;
     this.M_lenY= y;
     this.len= this.AddArray(x, y);
-    this.right= this.AddArray(x, y);
-    this.already= this.AddArray(x, y);
+    console.log(this.len);
     this.AddHTML(x, y);
 };
 App.prototype.AddArray = function (x, y){// return array
@@ -41,13 +43,13 @@ App.prototype.AddArray = function (x, y){// return array
     for (var i = 0; i < x; i++) {
         array[i]= [];
         for(var j= 0; j< y; j++){
-            array[i].push(0);
+            array[i].push([0, 0, 0]);//(mine, number), right, already
         }
     }
     return array;
 };
 App.prototype.AddHTML = function (x, y){// body.innerHTML, this.start();
-    var code="<div id='wrap'><ul id='plate'>";
+    var code="<div id='wrap'><div class='mask'></div><ul id='plate'>";
     for (var i = 0; i < x; i++) {
         code += `<li class='b'>`;
         for (var j = 0; j < y; j++) {
@@ -68,29 +70,86 @@ App.prototype.AddHTML = function (x, y){// body.innerHTML, this.start();
 };
 /* //난이도 선택 */
 /* 난이도 선택 후 */
+App.prototype.addEvent = function(event) {
+    var self= this;
+    for(var i=0; i<event.length; i++){
+        window.addEventListener(event[i], function (e){
+            e.preventDefault();
+            var _this= e.target;
+            if(_this.className=="c"){
+                var parent= e.target.parentNode.parentNode;
+                var x= [].slice.call(e.target.parentNode.children).indexOf(e.target);// this
+                var y= [].slice.call(parent.parentNode.children).indexOf(parent);// parent
+                switch(e.type) {
+                    case "click":
+                        if (self.isFirst) { //처음
+                            self.isFirst= false; //다음클릭부터는 처음이 아님
+                            self.MakeMine(x, y); //지뢰 생성
+                        }
+                        if(self.len[y][x][1] == 0 && self.len[y][x][1] == 0){
+                            self.onClick(x, y, _this);
+                            self.len[y][x][2] = 1;
+                        }
+                        break;
+                    case "mousedown":
+                        if (e.button == 0) {
+                            self.leftClick = true;
+                        }
+                        if (e.button == 2) {
+                            if (self.leftClick && self.len[y][x][1] == 0) {
+                                if(self.len[y][x][2] == 0)
+                                    return;
+                                var M=0;
+                                for(var i= -1; i<2; i++){
+                                    for(var j= -1; j<2; j++){
+                                        if(self.len[y + i] != undefined && self.len[y + i][x + j] != undefined && self.len[y + i][x + j][1] == 1){
+                                            M++;
+                                        }
+                                    }
+                                }
+                                if(Number(_this.innerText) == M){
+                                    self.NoNumber(x, y);
+                                }
+                            } else {
+                                if (self.len[y][x][2] == 0) {
+                                    if (self.len[y][x][1] == 0) {
+                                        self.len[y][x][1] = 1;
+                                        _this.style.background = 'rgb(10, 39, 69)';
+                                    } else {
+                                        self.len[y][x][1] = 0;
+                                        _this.style.background = 'rgb(45, 136, 231)';
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                    case "mouseup":
+                        if(!self.isFirst){
+                            var Mlen= self.M_len;
+                            for (var i = 0; i < self.M_lenY; i++) {
+                                for (var j = 0; j < self.M_lenX; j++) {
+                                    if (self.len[i][j][1] == 1 && self.len[i][j][0] == true) {
+                                        Mlen--;
+                                    }
+                                }
+                            }
+                            if (Mlen == 0) {// clear
+                                console.log('clear');
+                                self.end();
+                            }
+                        }
+                        if(e.button == 0)
+                            self.leftClick= false;
+                        break;
+                }
+            }
+        })
+    }
+};
 App.prototype.start = function (){
     var self= this;
     var block= document.querySelectorAll(".c");//지뢰 칸
-    for(var i= 0; i< block.length; i++){
-        block[i].addEventListener("click", function (){
-            var parent= this.parentNode.parentNode;
-            var cx= [].slice.call(this.parentNode.children).indexOf(this);// this
-            var cy= [].slice.call(parent.parentNode.children).indexOf(parent);// parent
-            if (self.isFirst) { //처음
-                self.isFirst= false; //다음클릭부터는 처음이 아님
-                self.MakeMine(cx, cy); //지뢰 생성
-            }
-            self.onClick(cx, cy, this);
-            this.style.background= "gray";
-            // if (self.right[cy][cx] === 0 || !right[cx][cy]) { //클릭한곳에 지뢰표시가 안돼있으면
-            //     already[cx][cy] = 1;
-            //     $(this).css({
-            //         background: 'gray'
-            //     });
-            //     click(cx, cy, $(this)); //클릭\
-            // }
-        });
-    }
+    this.addEvent(["click", "mousedown", "mouseup"]);
 };
 App.prototype.MakeMine = function(x, y, callback) {
     switch (this.M_lenY) {
@@ -115,10 +174,10 @@ App.prototype.addMine = function(x, y, i) { // 지뢰추가해줌
     if (this.CheckLocation(x, y, Rx, Ry)) {
         this.addMine(x, y, i);
     } else {
-        if (this.len[Ry][Rx] === true) {
+        if (this.len[Ry][Rx][0] === true) {
             this.addMine(x, y, i);
         } else {
-            this.len[Ry][Rx] = true;
+            this.len[Ry][Rx][0] = true;
             this.addNumber(Rx, Ry);
             return;
         }
@@ -145,24 +204,25 @@ App.prototype.CheckLocation = function(x, y, Rx, Ry) {//클릭한곳 주위인�
             return false;
     }
 };
-App.prototype.addNumber = function(x, y) {
+App.prototype.addNumber = function(x, y) {// 지뢰주위 숫자 증가
     for (var q = -1; q <= 1; q++) {
         for (var w = -1; w <= 1; w++) {
             if (y + q >= 0 && y + q < this.M_lenX) {
                 if (x + w >= 0 && x + w < this.M_lenY) {
-                    if (this.len[y + q][x + w] !== true)
-                        this.len[y + q][x + w]++;
+                    if (this.len[y + q][x + w][0] !== true)
+                        this.len[y + q][x + w][0]++;
                 }
             }
         }
     }
 };
 App.prototype.onClick = function(x, y, _this) {
-    switch (this.len[y][x]) {
+    switch (this.len[y][x][0]) {
         case false:
             break;
         case 0:
-            this.len[y][x] = false;
+            this.len[y][x][0] = false;
+            _this.style.background= "gray";
             this.NoNumber(x, y);
             break;
         case 1:
@@ -173,10 +233,12 @@ App.prototype.onClick = function(x, y, _this) {
         case 6:
         case 7:
         case 8:
-            _this.innerText= this.len[y][x];
+            _this.innerText= this.len[y][x][0];
+            _this.style.background= "gray";
             break;
         case true:
             console.log("지뢰클릭");
+            this.end();
             break;
         default:
             break;
@@ -194,6 +256,41 @@ App.prototype.NoNumber = function(x, y) {
             }
         }
     }
+};
+App.prototype.end = function() {
+    for(var i=0; i<this.M_lenY; i++){
+        for(var j=0; j<this.M_lenX; j++){
+            var _this= document.querySelectorAll("#plate>li")[i].querySelectorAll("li")[j];
+            console.log(_this);
+            switch (this.len[i][j][0]) {
+                case false:
+                    break;
+                case 0:
+                    _this.innerText= "";
+                    _this.style.backgroundColor= "gray";
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    _this.innerText= this.len[i][j][0];
+                    _this.style.backgroundColor= "gray";
+                    break;
+                case true:
+                    _this.innerText= "";
+                    _this.style.backgroundColor= "#000";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    console.log('a');
+    document.querySelector(".mask").style.display= "block";
 };
 /* //난이도 선택 후 */
 window.onload= function (){
@@ -303,93 +400,5 @@ function click(clx, cly, cli) {
             break;
         default:
             break;
-    }
-}
-
-function no(nx, ny) {
-    for (var i = -1; i <= 1; i++) {
-        for (var j = -1; j <= 1; j++) {
-            if (nx + i >= 0 && nx + i < x) {
-                if (ny + j >= 0 && ny + j < y) {
-                    if (i != 0 || j != 0) {
-                        $('#plate>li').eq(nx + i).find('li').eq(ny + j).click();
-                    }
-                }
-            }
-        }
-    }
-}
-let mine_length; // 지뢰개수
-let mx;
-let my;
-
-function mine(n, n2) { //x, y
-    switch (y) {
-        case 9:
-            mine_length = 10;
-            break;
-        case 16:
-            mine_length = 40;
-            break;
-        case 30:
-            mine_length = 99;
-            break;
-        default:
-    }
-    for (i = 0; i < mine_length; i++) { //지뢰개수만큼 for문을 돌림
-        makemine(n, n2, i);
-    }
-}
-
-function makemine(mmx, mmy, i) {
-    mx = Math.floor(Math.random() * (x)); //랜덤좌표
-    my = Math.floor(Math.random() * (y)); //랜덤좌표
-    if (where(mx, my, mmx, mmy)) {
-        makemine(mmx, mmy);
-    } else {
-        if (len[mx][my] === true) {
-            makemine(mmx, mmy);
-        } else {
-            // 지금만확인
-            // $('#plate>li').eq(mx).find('li').eq(my).css({background:'#000'});
-            // 지금만확인
-            return len[mx][my] = true, number(mx, my);
-        }
-    }
-}
-
-function where(wx, wy, cwx, cwy) {
-    switch (cwx - wx) {
-        case -1:
-        case 0:
-        case 1:
-            switch (cwy - wy) {
-                case -1:
-                case 0:
-                case 1:
-                    return true;
-                    break;
-                default:
-            }
-            break;
-        default:
-            return false;
-    }
-}
-
-function number(nx, ny) { //8,7
-    for (var q = -1; q <= 1; q++) {
-        for (var w = -1; w <= 1; w++) {
-            if (nx - q >= 0 && nx - q < x) {
-                if (ny - w >= 0 && ny - w < y) {
-                    if (len[nx - q][ny - w] !== true) { //len[7][6]len[7][7]len[7][8]
-                        len[nx - q][ny - w]++;
-                        // 지금만 확인
-                        // $('#plate>li').eq(nx-q).find('li').eq(ny-w).text(len[nx-q][ny-w]);
-                        // 지금만 확인
-                    }
-                }
-            } else {}
-        }
     }
 }
